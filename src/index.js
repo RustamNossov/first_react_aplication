2b9385a25a7a5240c0b88d2e786c71e350633610
RustamNossov <rustamn48@gmail.com>
2021-06-24 20:05:49 +0600

    Web app components added

--- src/index.js
+++ src/index.js
@@ -1,63 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-//import reportWebVitals from './reportWebVitals';
-
-// const elem = (
-//   <div>
-//     <h2>Hello world!</h2>
-//     <input type="text" placeholder = "Type here..." />
-//     <button/>
-//   </div>
-
-// )
-
-const Header = () => {
-  const scr = 'Hello world';
-  return <h2>{scr}</h2>
-}
-
-const Field = () => {
-  const holder = "Enter here";
-  const styledField = {
-            width: '300px'
-          }
-  return <input 
-    style = {styledField}
-    type="text" 
-    placeholder = {holder} 
-    autoComplete = ""
-    className="first"
-    htmlFor=""/>
-}
-
-const Button = () => {
-  const text = "log in";
-  const res = () => {
-    return 'Log in, please'
-  }
-  const p = <p>Log in</p>
-  const logged = false;
-  // return <button>{res()}</button>
-  //return <button>{p}</button>
-  return <button>{logged ? null : text}</button>
-}
-
-const App = () => {
-  return (
-    <div> 
-      <Header/>
-      <Field/>
-      <Button/>
-    </div>
-  )
-}
+import App from './components/app/';
 
 
 ReactDOM.render(<App/>,document.getElementById('root'));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
